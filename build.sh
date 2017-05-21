8f92df9a9ccda307ef1f48859fbb28f696b36b27
Josh <josh.barnes@allocadia.com>
2017-05-20 19:20:10 -0700

    Added version to compose file

--- build.sh
+++ build.sh
@@ -4,24 +4,6 @@ cd $TRAVIS_BUILD_DIR/site
 yarn install
 
 cd $TRAVIS_BUILD_DIR
-#sudo apt-get update
-#sudo apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y docker-engine
-  
-# Print out the current docker-compose version. Once this reports 1.6+ then we
-# do not need the following steps.
-docker-compose --version
-
-# As of the writing of this script Travis has docker-compose v1.4.2, we need
-# to update it to 1.8+. $DOCKER_COMPOSE_VERSION is provide from the `env`
-# above.
-#sudo rm /usr/local/bin/docker-compose
-#curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-#chmod +x docker-compose
-#sudo mv docker-compose /usr/local/bin
-
-# Check that docker-compose is now running the latest version (or at least the
-# one we specified). This is not to be confused with the version we printed
-# before doing the update.
 docker-compose --version
 
 pip install --user awscli
